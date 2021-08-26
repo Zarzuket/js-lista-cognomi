d1baf03a8ceccba7f6e6cc5dfdcddad5e8800a08
Zarzuket <davide.bajo@gmail.com>
2021-08-26 16:01:39 +0200

    add exercise

--- js/script.js
+++ js/script.js
@@ -2,3 +2,19 @@
 // chiedi all'utente il cognome e pushalo sull'array
 // stampa la lista in ordine alfabetico
 // posiziona umana (partendo da 1) 
+
+var listaCognomi = ["Bianchi" , "Neri" , "Rossi" , "Verdi" , "Gialli"];
+
+var cognomeUtente = prompt("Inserisci il tuo cognome");
+
+cognomeUtente = cognomeUtente.toUpperCase() + cognomeUtente.substring(1);
+listaCognomi.push(cognomeUtente);
+
+// console.log(listaCognomi);
+
+
+// console.log(listaCognomi.sort());
+var listaCognomiUp = listaCognomi.sort();
+console.log(listaCognomiUp)
+
+console.log(listaCognomiUp.indexOf(cognomeUtente)+1);
